b0a628f651e3fd9326d293330e19cce044317d0f
Juliia3 <yulya.sobchenko2001@gmail.com>
2023-01-09 15:46:31 +0200

    all

---
+++ spa boutique/src/components/Footer/Footer.vue
@@ -0,0 +1,12 @@
+<script setup>
+import './footer.scss'
+</script>
+
+<template>
+    <footer class="footer">
+        <div class="footer__container container">
+            <RouterLink to="/" target="_blank"><h1 class="header__logo">Boutique</h1></RouterLink>
+            <p class="footer__text">A wide range of products that do not harm the planet, but effectively keep your skin in tone, is right here.</p>
+        </div>
+    </footer>
+</template>
